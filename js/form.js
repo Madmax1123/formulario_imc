5c4a18e916f846d9f594fba0cbd3dca1c0cb2a2f
Lorenzo Soares <lorenzodavi876@gmail.com>
2021-03-02 12:28:19 -0300

    commit

--- js/form.js
+++ js/form.js
@@ -6,17 +6,36 @@ botaoAdicionar.addEventListener("click", function(){
     var paciente = obtemPacienteDoFormulario(form)    
 
     var pacienteTr = montaTr(paciente);
-    
+
+    var erros = validaPaciente(paciente);
+    if(erros.length > 0 ){
+        exibeMensagensDeErro(erros);
+        return;
+    }
+
     var tabela = document.querySelector("#tabela-pacientes")
 
     tabela.appendChild(pacienteTr);
     
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro")
+    mensagensErro.innerHTML = "";
 
 });
 
+function exibeMensagensDeErro(erros){
+
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li)
+    });
+}
+
 function obtemPacienteDoFormulario(form){
-    
+
     var paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
@@ -47,4 +66,35 @@ function montaTd(dado,classe){
     td.textContent = dado;
     td.classList.add(classe)
     return td;
+}
+
+
+function validaPaciente(paciente){
+    
+    var erros = [];
+    
+    if( paciente.nome.length == 0){
+        erros.push("O nome não pode estar em branco!");
+    }
+
+    if(!validaPeso(paciente.peso)){
+       erros.push("Peso é inválido!");
+    }
+ 
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura e inválida!");
+    }
+    
+    if( paciente.gordura.length == 0){
+        erros.push("A gordura não pode estar em branco!")
+    }
+    
+    if( paciente.peso.length == 0){
+        erros.push("O peso não pode estar em branco!")
+    }
+    
+    if( paciente.altura.length == 0){
+        erros.push("A altura não pode estar em branco!")
+    }
+    return erros;
 }
