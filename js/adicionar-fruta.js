d1e854c064076a72448fb5b0029f4a2000417bec
Lorenzo Soares <lorenzodavi876@gmail.com>
2021-03-04 17:50:31 -0300

    Qualquer porcaria

---
+++ js/adicionar-fruta.js
@@ -0,0 +1,90 @@
+var botaoAdicionar = document.querySelector("#adicionar-fruta");
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona")
+    var Fruta = obtemFrutaDoFormulario(form)    
+
+    var FrutaTr = montaTr(Fruta);
+
+   
+    var tabela = document.querySelector("#tabela-frutas")
+
+    tabela.appendChild(FrutaTr);
+    
+    form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro")
+    mensagensErro.innerHTML = "";
+
+});
+
+function exibeMensagensDeErro(erros){
+
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li)
+    });
+}
+
+function obtemFrutaDoFormulario(form){
+
+    var fruta = {
+        Nome: form.Nome.value,
+        Valor: form.Valor.value,
+        Quantidade: form.Quantidade.value,
+        Total: calculaTotal(form.Valor.value, form.Quantidade.value)
+    }
+   
+    return fruta;
+}
+
+function montaTr(fruta){
+    var frutaTr = document.createElement("tr")
+    frutaTr.classList.add("produtos");
+
+    frutaTr.appendChild(montaTd(fruta.Nome, "info-Nome"));
+    frutaTr.appendChild(montaTd(fruta.Valor, "info-Valor"));
+    frutaTr.appendChild(montaTd(fruta.Quantidade, "info-Quantidade"));
+    frutaTr.appendChild(montaTd(fruta.Total, "info-Total"));
+    
+
+    return frutaTr
+}
+
+
+function montaTd(dado,classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe)
+    return td;
+}
+
+
+function validaFruta(Fruta){
+    
+    var erros = [];
+    
+    if( Fruta.Nome.length == 0){
+        erros.push("O nome não pode estar em branco!");
+    }
+
+    if(!validaValor(Fruta.Valor)){
+       erros.push("Valor é inválido!");
+    }
+ 
+    if(!validaQuantidade(Fruta.Quantidade)){
+        erros.push("A quantidade é inválida!");
+    }
+    
+    if( Fruta.Valor.length == 0){
+        erros.push("O valor não pode estar em branco!")
+    }
+    
+    if( Fruta.Quantidade.length == 0){
+        erros.push("A quantidade não pode estar em branco!")
+    }
+    return erros;
+}
---
+++ js/calcula-valor.js
@@ -0,0 +1,41 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Supermercado virtual";
+
+var Frutas = document.querySelectorAll(".fruta");
+
+for(var i = 0; i < Frutas.length ; i++){
+    
+    var Fruta = Frutas[i];
+
+    var tdValor= Fruta.querySelector(".info-Valor");
+    var Valor = tdValor.textContent;
+    
+
+
+    var tdQuantidade = Fruta.querySelector(".info-Quantidade");
+    var Quantidade = tdQuantidade.textContent;
+
+
+
+    var tdTotal =  Fruta.querySelector(".info-Total");
+
+
+   
+
+
+ 
+        var Total = calculaTotal(Valor,Quantidade);
+        tdTotal.textContent = Total;
+ 
+    }
+
+
+
+function calculaTotal(Valor,Quantidade){
+    var Total = 0;
+
+    Total = (Valor * Quantidade) 
+  
+
+    return Total;
+}
